2a8bf20408a119a33df73b2338822ff500f23a1e
Mateus <fcamidumateus345@gmail.com>
2025-09-07 15:57:39 -0300

    Add files via upload

---
+++ Ex4/Ex4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo phineas = new Triangulo();
+
+
+            Console.WriteLine("Digite o valor da BASE do triângulo:");
+            phineas.setBase1(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite o valor da ALTURA do triângulo:");
+            phineas.setAltura(double.Parse(Console.ReadLine()));
+
+
+
+            phineas.calculaArea();
+
+            Console.WriteLine("O valor da área do triângulo é: {0}", phineas.getArea());
+        }
+    }
+}
+
